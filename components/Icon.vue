8b2c87115546d2f74b3d81d757d2369208f16e63
Brandon Liu <bdon@users.noreply.github.com>
2024-10-09 14:13:35 +0800

    icons display in dark mode (#77)
    * icons display in dark mode

--- components/Icon.vue
+++ components/Icon.vue
@@ -1,26 +1,40 @@
 <script setup lang="ts">
 import { ref, onMounted, onUpdated, watch } from "vue";
+import { useData } from "vitepress";
+const { isDark } = useData();
 
 const canvasRef = ref(null);
 
 const props = defineProps<{
-  kind?: string;
-  sprites?: Promise<[any, any]>;
+  kind: string;
+  sprites: Promise<[unknown, HTMLImageElement, unknown, HTMLImageElement]>;
 }>();
 
-onMounted(async () => {
+const drawIcon = async () => {
   const ctx = canvasRef.value.getContext("2d");
-  const [j, i] = await props.sprites;
-  if (props.kind in j) {
-    const data = j[props.kind];
-    ctx.drawImage(i, data.x, data.y, data.width, data.height, 0, 0, 38, 38);
+  ctx.clearRect(0, 0, 38, 38);
+  const sprites = await props.sprites;
+  const json = isDark.value ? sprites[2] : sprites[0];
+  const image = isDark.value ? sprites[3] : sprites[1];
+
+  if (props.kind in json) {
+    const data = json[props.kind];
+    ctx.drawImage(image, data.x, data.y, data.width, data.height, 0, 0, 38, 38);
   } else {
     ctx.strokeStyle = "steelblue";
     ctx.setLineDash([2, 2]);
-    ctx.strokeWidth = 2;
+    ctx.strokeWidth = 4;
     ctx.rect(0, 0, 38, 38);
     ctx.stroke();
   }
+};
+
+onMounted(async () => {
+  drawIcon();
+});
+
+watch(isDark, () => {
+  drawIcon();
 });
 </script>
 
