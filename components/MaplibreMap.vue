89c19f9c27864774d5cb49e9e217afe11a61c3bb
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2024-10-09 07:15:54 +0200

    Add RTL plugin (#76)
    * Add RTL plugin

--- components/MaplibreMap.vue
+++ components/MaplibreMap.vue
@@ -103,6 +103,12 @@ const props = defineProps<{
 }>();
 
 onMounted(() => {
+  if (maplibregl.getRTLTextPluginStatus() === "unavailable") {
+    maplibregl.setRTLTextPlugin(
+      "https://unpkg.com/@mapbox/mapbox-gl-rtl-text@0.2.3/mapbox-gl-rtl-text.js",
+      false,
+    );
+  }
   map = new maplibregl.Map({
     container: mapRef.value,
     style: style(props.theme, props.highlightLayer),
